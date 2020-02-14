9018d48bd78f29678c840cbc75b4d7d31e6531d7
loginashok <60283219+loginashok@users.noreply.github.com>
2020-02-14 17:52:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import topnav from './component/topnav';
+
+
+function App() {
+    return(<topnav></topnav>);
+ 
+}
+
+export default App;
---
+++ src/component/topnav.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../index.css';
+
+function topnav (){
+   
+     
+        return (<p>this is react</p>
+    
+       /*  <div className="container center">
+           <nav className="menu">
+               <h1 className="menu__logo">wave global</h1>
+   
+               <div className="menu__right">
+                   <ul className="menu__list">
+                       <li className="menu__list-item"><a className="menu__link menu__link--active" href="#">Home</a></li>
+                       <li className="menu__list-item"><a className="menu__link" href="#">About</a></li>
+                       <li className="menu__list-item"><a class="menu__link" href="#">Portfo</a></li>
+                       <li className="menu__list-item"><a class="menu__link" href="#">Contact</a></li>
+                   </ul>
+   
+                   <button className="menu__search-button"></button>
+   
+                   <form className="menu__search-form hide" method="POST">
+                       <input className="menu__search-input" placeholder="Type and hit enter" />
+                   </form>
+               </div>
+           </nav>
+       </div> */
+      
+     )
+
+}
+
+export default topnav;
